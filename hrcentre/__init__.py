adc4dd81ed26a69aa55c338a5605cfe23f70d177
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2025-07-16 13:39:32 +0000

    updated django esi to v7

--- hrcentre/__init__.py
+++ hrcentre/__init__.py
@@ -3,3 +3,6 @@
 __author__ = """Matteo Ghia"""
 __email__ = 'matteo.ghia@yahoo.it'
 __version__ = '0.0.1b2'
+
+__app_name_ua__ = 'aa-hr-centre'
+__github_url__ = 'https://github.com/Maestro-Zacht/aa-hr-centre'
